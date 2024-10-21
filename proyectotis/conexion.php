9581e109a683335c133bdef3aec62e7a32b35d07
feliger27 <131935293+feliger27@users.noreply.github.com>
2024-10-21 19:41:45 -0300

    Delete proyectotis directory

--- proyectotis/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conexion = mysqli_connect("localhost","root","","hamburgeeks");
-    //servidor,user,mysql,pass mysql, bdname
-    if($conexion->connect_error) {
-        die("Conexión Fallida: ". $conn->connect_error);
-    }
-?>
